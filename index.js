910034f1208f2a86636b1ff418074e1790cc9839
myname-abhik <abhik16chakrabortty@gmail.com>
2024-05-16 13:26:23 +0530

    Cart_available

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ app.use(fileUpload({
     
 }))
 app.use(cors());
-app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.use(cookieParser())
+app.use(express.urlencoded({extended:false}));
 
 
 
