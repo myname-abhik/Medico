577d5a70d7681d3b7a8a5268960df03ccb033958
myname-abhik <abhik16chakrabortty@gmail.com>
2025-03-17 17:44:51 +0530

    comit

--- connect.js
+++ connect.js
@@ -6,7 +6,8 @@ async function connectToMongoDB() {
   // const PASSWORD = dtTA8fYR0bzsKdZj
   mongoose
     .connect(
-      `mongodb+srv://abhik16chakrabortty:D0HsLzUvxx2GFcUu@cluster0.iuakazn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+      // `mongodb+srv://abhik16chakrabortty:D0HsLzUvxx2GFcUu@cluster0.iuakazn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+      `mongodb+srv://Abhik:1213@cluster0.bbxtgyl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
     )
     .then(() => {
       console.log("Connected to MongoDB");
